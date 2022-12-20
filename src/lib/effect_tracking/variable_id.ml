4d905733ac1f51377f948d8a2cd39623ed145a2d
Dylan Moss <dylan.moss2001@gmail.com>
2022-12-20 12:50:55 +0000

    Pre environment redesign

--- src/lib/effect_tracking/variable_id.ml
+++ src/lib/effect_tracking/variable_id.ml
@@ -16,7 +16,6 @@ module Var_uuid_annotation = struct
   type t = var_uuid [@@deriving compare, sexp_of, of_sexp]
 
   let string_of_t t = t.name
-  let create name uuid = { name; uuid }
 end
 
 module String_item = struct
@@ -56,129 +55,20 @@ end
 module Variable_uuid_ast_mapping = struct
   include Default_ast_mapping (Unknown_variable_uuid_ast_mapping)
 
-  (* HANDLE EXCEPTIONS *)
+  (* HANDLE EXCEPTIONS : FROM ENVIRONMENT AS WELL *)
 
   exception Unbound_var of string
 
-  (* let create_new_uuid ~env ~var ~result ~constructor =
-    let _ =
-      Var_uuid_environment.get_value_outside_scope
-        (String_item.create var.name)
-        env
-    in
-    let uuid = Uuid.create () in
-    ( add_to_env (String_item.create var.name) uuid env
-    , constructor { var with uuid }
-    , result )
-
-
-  let use_existing_uuid ~env ~var ~result ~constructor =
-    match Var_uuid_environment.get_value (String_item.create var.name) env with
-    | Some uuid -> env, constructor { var with uuid }, result
-    | None -> raise (Unbound_var var.name) *)
-
   let new_var env (var : old_var_annot) =
-    let _ =
-      Var_uuid_environment.get_value_outside_scope
-        (String_item.create var.name)
-        env
-    in
+    let _ = Var_uuid_environment.get_value_outside_scope var.name env in
     let uuid = Uuid.create () in
-    ( add_to_env (String_item.create var.name) uuid env
-    , { name = var.name; uuid }
-    , empty_result () )
+    add_to_env var.name uuid env, { name = var.name; uuid }, empty_result ()
 
 
   let existing_var env (var : old_var_annot) =
-    match Var_uuid_environment.get_value (String_item.create var.name) env with
+    match Var_uuid_environment.get_value var.name env with
     | Some uuid -> env, { name = var.name; uuid }, empty_result ()
     | None -> raise (Unbound_var var.name)
-
-  (* let expr env expr result =
-    match expr with
-    | VarRead var ->
-      use_existing_uuid ~env ~var ~result ~constructor:(fun x -> VarRead x)
-    | _ -> ignore_branch env expr result
-
-
-  let var_statement env var_statement result =
-    match var_statement with
-    | VarNonInit (var, type_id) ->
-      create_new_uuid ~env ~var ~result ~constructor:(fun x ->
-          VarNonInit (x, type_id))
-    | VarInit (var, type_id, expr) ->
-      create_new_uuid ~env ~var ~result ~constructor:(fun x ->
-          VarInit (x, type_id, expr))
-    | VarDecl (var, expr) ->
-      create_new_uuid ~env ~var ~result ~constructor:(fun x ->
-          VarDecl (x, expr))
-    | VarAssign (var, expr) ->
-      create_new_uuid ~env ~var ~result ~constructor:(fun x ->
-          VarAssign (x, expr))
-    | Pre_inc var ->
-      use_existing_uuid ~env ~var ~result ~constructor:(fun x -> Pre_inc x)
-    | Pre_dec var ->
-      use_existing_uuid ~env ~var ~result ~constructor:(fun x -> Pre_dec x)
-    | Post_inc var ->
-      use_existing_uuid ~env ~var ~result ~constructor:(fun x -> Post_inc x)
-    | Post_dec var ->
-      use_existing_uuid ~env ~var ~result ~constructor:(fun x -> Post_dec x)
-
-
-  let user_func env (user_func : new_var_annot user_func) result =
-    use_existing_uuid ~env ~var:user_func.name ~result ~constructor:(fun x ->
-        { user_func with name = x })
-
-
-  let write_template env (write_template : new_var_annot write_template) result =
-    use_existing_uuid
-      ~env
-      ~var:write_template.file
-      ~result
-      ~constructor:(fun x -> { write_template with file = x })
-
-
-  let for_each env for_each result =
-    let _ =
-      Var_uuid_environment.get_value_outside_scope
-        (String_item.create for_each.item.name)
-        env
-    in
-    let uuid = Uuid.create () in
-    let new_env = add_to_env (String_item.create for_each.item.name) uuid env in
-    let new_item = { result.item with uuid } in
-    match
-      Var_uuid_environment.get_value
-        (String_item.create for_each.iterator.name)
-        env
-    with
-    | Some uuid ->
-      ( new_env
-      , { for_each with
-          item = new_item
-        ; iterator = { result.iterator with uuid }
-        }
-      , result )
-    | None -> raise (Unbound_var for_each.iterator.name)
-
-
-  let param env (var, type_id) result =
-    create_new_uuid ~env ~var ~result ~constructor:(fun x -> x, type_id)
-
-
-  let func env (func : ('a, new_var_annot) func) result =
-    create_new_uuid ~env ~var:func.name ~result ~constructor:(fun x ->
-        { func with name = x })
-
-
-  let program ~env ~new_package ~old_import ~new_global_vars ~new_funcs ~result =
-    ( env
-    , { package = new_package
-      ; imports = old_import
-      ; global_vars = new_global_vars
-      ; funcs = new_funcs
-      }
-    , result ) *)
 end
 
 module Variable_uuid_ast_pipeline = Ast_pipeline (Variable_uuid_ast_mapping)
